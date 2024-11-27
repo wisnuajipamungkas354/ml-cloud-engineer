38f1802974f232682e644f6d99cd3e24335d2689
Wisnu Aji Pamungkas <wisnuajipamungkas354@gmail.com>
2024-11-28 06:14:52 +0700

    First commit frontend

--- src/histories.js
+++
@@ -1,3 +0,0 @@
-const histories = [];
-
-export default histories;
--- src/loadModel.js
+++
@@ -1,8 +0,0 @@
-import tfjs from '@tensorflow/tfjs-node';
-
-function loadModel() {
-  const modelUrl = process.env.MODEL_URL;
-  return tfjs.loadGraphModel(modelUrl);
-}
-
-export default loadModel;
